e0186ee44bd2c2874d9d3b0aa80c68b72e051c37
Isaac <adetonaisaac12345@gmail.com>
2020-10-06 23:29:01 +0100

    Added Comment using disqus

--- src/templates/chapter-details.js
+++ src/templates/chapter-details.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { DiscussionEmbed } from "disqus-react"
 import Layout from '../components/Layout';
 import ChapterDetailsHero from '../components/ChapterDetailsHero/ChapterDetailsHero'
 import ChapterDetailsBody from '../components/ChapterDetailsBody/ChapterDetailsBody';
+import styles from '../styles/templates-styles.module.scss';
 
 export const data = graphql`
 query($slug:String){
@@ -23,13 +25,21 @@ query($slug:String){
     `
 
 
+
 const ChapterDetails = (props) => {
 
+    const disqusConfig = {
+        shortname: process.env.GATSBY_DISQUS_NAME,
+        config: { identifier:props.data.contentfulSeriesChapter.chapterSlug},
+    }
 
     return(
         <Layout>
             <ChapterDetailsHero chapterNumber={props.data.contentfulSeriesChapter.chapterNumber} />
             <ChapterDetailsBody chapterImages={props.data.contentfulSeriesChapter.chapterImages} chapterTitle={props.data.contentfulSeriesChapter.chapterTitle}/>
+            <div className={styles.comment_container}>
+                <DiscussionEmbed {...disqusConfig} />
+            </div>
         </Layout>
     )
 }
