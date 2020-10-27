6e7f736ef00e4a504992609a233230b38a8bcafe
Isaac <adetonaisaac12345@gmail.com>
2020-10-27 16:24:43 +0100

    change hero text

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -26,7 +26,7 @@ class HomeHero extends React.Component {
                     <div className={homeheroStyles.homeheroContainer}>
                         <div className={homeheroStyles.homeheroContent}>
                             <h1>Explore various collection of <span>COMIC SERIES</span></h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid maxime amet quaerat laboriosam inventore unde.</p>
+                            <p>Browse through the app, Select a comic series from our wide range collection, relax and enjoy beautiful contents</p>
                             <div className={homeheroStyles.button}>
                                 <button>View Collections</button>
                             </div>
@@ -37,7 +37,7 @@ class HomeHero extends React.Component {
                     <div className={homeheroStyles.homeheroContainer}>
                         <div className={homeheroStyles.homeheroContent}>
                             <h1>Explore various collection of <span>COMIC STRIPS</span></h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid maxime amet quaerat laboriosam inventore unde.</p>
+                            <p>Browse through the app, Select a comic series from our wide range collection, relax and enjoy beautiful contents</p>
                             <div className={homeheroStyles.button}>
                                 <button>View Collections</button>
                             </div>
@@ -48,7 +48,7 @@ class HomeHero extends React.Component {
                     <div className={homeheroStyles.homeheroContainer}>
                         <div className={homeheroStyles.homeheroContent}>
                             <h1>Explore various collection of <span>Digital Arts</span></h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid maxime amet quaerat laboriosam inventore unde.</p>
+                            <p>Browse through the app, Select a comic series from our wide range collection, relax and enjoy beautiful contents</p>
                             <div className={homeheroStyles.button}>
                                 <button>View Collections</button>
                             </div>
